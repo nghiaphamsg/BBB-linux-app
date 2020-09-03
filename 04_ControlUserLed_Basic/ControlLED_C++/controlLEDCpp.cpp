0987a4c544f428c91622162f9ee8ab05a15fff6d
nghiaphamsg <nghiaphamsg@gmail.com>
2020-09-04 01:05:47 +0900

    Fix bug when switch between on and flash

--- 04_ControlUserLed_Basic/ControlLED_C++/controlLEDCpp.cpp
+++ 04_ControlUserLed_Basic/ControlLED_C++/controlLEDCpp.cpp
@@ -2,17 +2,17 @@
  ============================================================================
  Name        : ControlLED.cpp
  Author      : NghiaPham
- Version     :	v0.1
+ Version     : v0.1
  Copyright   : Your copyright notice
- Description : Control USER LED 3 (internal Led) in BBB, write by cpp
+ Description : Control USER LED 0, 1, 2, 3 (internal Led) in BBB, write by cpp
  ============================================================================
  */
 
 
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<sstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 #define PATH "/sys/class/leds/beaglebone:green:usr"
 
@@ -21,9 +21,9 @@ class LED {
 		int pinNumber;
 		std::string statePath;
 		virtual void writeStateLED(std::string fileName, std::string value);
-		virtual void removeTrigger();
 	public:
 		LED(int number);
+		virtual void removeTrigger();
 		virtual void turnOnLED();
 		virtual void turnOffLED();
 		virtual void flashLED(std::string delayms);
@@ -49,38 +49,39 @@ void LED::removeTrigger() {
 }
 
 void LED::turnOnLED() {
-	std::cout << "<<<DEBUG START>>> Turning LED " << pinNumber << " on." << std::endl;
+	std::cout << "Turning LED " << pinNumber << " on." << std::endl;
 	writeStateLED("/brightness", "1");
 }
 
 void LED::turnOffLED() {
-	std::cout << "<<<DEBUG START>>> Turning LED " << pinNumber << " off." << std::endl;
+	std::cout << "Turning LED " << pinNumber << " off." << std::endl;
 	writeStateLED("/brightness", "0");
 }
 
 void LED::flashLED(std::string delayms = "50") {
-	std::cout << "<<<DEBUG START>>> Making LED " << pinNumber << " flash." << std::endl;
+	std::cout << "Making LED " << pinNumber << " flash." << std::endl;
 	writeStateLED("/trigger", "timer");
 	writeStateLED("/delay_on", delayms);
 	writeStateLED("/delay_off", delayms);
 }
 
 LED::~LED() {
-	std::cout << "<<<DEBUG START>>> Destroying the LED with path: " << statePath << std::endl;
+	std::cout << "Destroying the LED with path: " << statePath << std::endl;
 }
 
 int main(int argc, char* argv[]) {
 	if(argc != 2) {
-		std::cout << "<<<DEBUG START>>> Usage is makeLEDs <command>" << std::endl;
-		std::cout << "<<<DEBUG START>>> Command is one of: on, off, and flash" << std::endl;
-		std::cout << "<<<DEBUG START>>> e.g. makeLEDs flash" << std::endl;
+		std::cout << "	Usage: "<< argv[0] <<" <command>" << std::endl;
+		std::cout << "	command is one of: <on> <off> or <flash>" << std::endl;
+		std::cout << "	e.g. makeLEDs flash" << std::endl;
 	}
 
-	std::cout << "<<<DEBUG START>>> Starting the makeLEDs program" << std::endl;
+	std::cout << "Starting the makeLEDs program" << std::endl;
 	std::string cmd{argv[1]};
 	LED leds[4] = {LED(0), LED(1), LED(2), LED(3)};
 	for(int i=0; i<=3; i++) {
 		if(cmd == "on") {
+			leds[i].removeTrigger();
 			leds[i].turnOnLED();
 		}
 		else if (cmd == "off") {
@@ -90,9 +91,9 @@ int main(int argc, char* argv[]) {
 			leds[i].flashLED("150");
 		}
 		else {
-			std::cout << "<<<DEBUG START>>> Invalid command!" << std::endl;
+			std::cout << "Invalid command!" << std::endl;
 		}
 	}
-   std::cout << "<<<DEBUG START>>> Finished the makeLEDs program" << std::endl;
+   std::cout << "Finished the makeLEDs program" << std::endl;
    return 0;
 }
